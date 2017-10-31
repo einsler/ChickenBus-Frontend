7b1105b486f6a8aa075e55da40a15fa18eaf1f42
robobigfoot <ericnew@live.unc.edu>
2017-10-31 17:01:26 -0400

    Log Content

--- src/components/LogContent/LogContent.tsx
+++ src/components/LogContent/LogContent.tsx
@@ -1,29 +1,31 @@
-import * as React from 'react';
-import {
-    ILogContent,
-    ILogContentProps,
-    ILogContentStyles
-} from './LogContent.Props';
-import { BaseComponent, autobind } from 'office-ui-fabric-react/lib/Utilities';
-import { getStyles } from './LogContent.styles'
-
-
-const styles = getStyles();
+import {  ILogContent,  ILogContentProps,  ILogContentStyles } from './LogContent.Props';
+import { BaseComponent, getRTL } from "office-ui-fabric-react/lib/Utilities";
+import { List } from "office-ui-fabric-react/lib/List";
+import { SearchBox } from "office-ui-fabric-react/lib/SearchBox";
+import { TextField } from "office-ui-fabric-react/lib/TextField";
+import { Label } from "office-ui-fabric-react/lib/Label";
+import { PrimaryButton, IButtonProps } from "office-ui-fabric-react/lib/Button";
+import { getStyles } from "./LogContent.styles";
+import { Image, ImageFit } from "office-ui-fabric-react/lib/Image";
+import { Icon } from "office-ui-fabric-react/lib/Icon";
+import * as React from "react";
 
 interface ILogContentState {
 }
+const styles: ILogContentStyles = getStyles();
 
 export class LogContent extends BaseComponent<ILogContentProps, ILogContentState> {
-
     constructor(props: ILogContentProps) {
         super(props);
+        this.state = {
+        }
     }
 
     public render() {
-      return(
-        <div style={styles.root}>
-          This space is reserved for a collapsible log for all entered bus route information, for review and management.
-        </div>
-      )
+        return(
+          <div style={ styles.root }>
+              This page is reserved for Log Content
+          </div>
+        )
     }
 }
